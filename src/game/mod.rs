5b8cbe9d29fe3bcee095968bf5c28b3191cfeacf
Theemathas Chirananthavat <theemathas@gmail.com>
2014-12-27 17:27:19 +0700

    Export stuff out of mod game.

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -2,6 +2,20 @@
 
 #![allow(dead_code)]
 
+pub use self::color::{Color, White, Black};
+pub use self::piece::Piece;
+pub use self::piece::Type as PieceType;
+pub use self::piece::{WP,WK,WQ,WB,WN,WR};
+pub use self::piece::{BP,BK,BQ,BB,BN,BR};
+pub use self::piece::Type::{Pawn,King,Queen,Bishop,Knight,Rook};
+pub use self::moves::{Move, Plies};
+pub use self::pos::Position;
+pub use self::fen::{fen_to_position, start_pos};
+pub use self::legal::receive_legal;
+pub use self::make_move::{make_move, make_move_mut};
+pub use self::mate::{is_checkmated, is_stalemated};
+pub use self::mate::{is_checkmate, is_stalemate};
+
 mod color;
 mod piece;
 mod square;

e147c7365d3fb3201392f43b9b2ea934f82b80d1
Theemathas Chirananthavat <theemathas@gmail.com>
2015-01-02 12:15:34 +0700

    Edit state module.

--- src/state/mod.rs
+++ src/state/mod.rs
@@ -9,21 +9,18 @@ use types::NumNodes;
 
 pub use self::TimeData::{TimeLeft, ExactTime};
 
+#[deriving(Clone)]
 pub struct State {
     pub is_debug: bool,
-    pub my_side: Color,
     pub pos: Position,
     pub start_search: u64,
     pub search_param: SearchParam,
     pub time_data: TimeData,
 }
-impl State {
-    pub fn is_my_turn(&self) -> bool {
-        self.my_side == self.pos.side_to_move()
-    }
-}
 
+#[deriving(Clone)]
 pub struct SearchParam {
+    pub ponder: Option<(Position, Move)>,
     pub search_moves: Option<Vec<Move>>,
     pub depth: Option<NumPlies>,
     pub nodes: Option<NumNodes>,
@@ -43,4 +40,30 @@ pub struct TimeLeftData {
     pub w_inc: Duration,
     pub b_inc: Duration,
     pub moves_to_go: NumMoves,
+}
+impl TimeLeftData {
+    pub fn time(&self, c: Color) -> Duration {
+        match c {
+            Color::White => self.w_time,
+            Color::Black => self.b_time,
+        }
+    }
+    pub fn set_time(&mut self, c: Color, val: Duration) {
+        match c {
+            Color::White => self.w_time = val,
+            Color::Black => self.b_time = val,
+        }
+    }
+    pub fn inc(&self, c: Color) -> Duration {
+        match c {
+            Color::White => self.w_inc,
+            Color::Black => self.b_inc,
+        }
+    }
+    pub fn set_inc(&mut self, c: Color, val: Duration) {
+        match c {
+            Color::White => self.w_inc = val,
+            Color::Black => self.b_inc = val,
+        }
+    }
 }

0c3249a9c7f9379a42a3df3cdac6f301be163ce7
Theemathas Chirananthavat <theemathas@gmail.com>
2015-01-03 12:41:12 +0700

    Implement stub for process().

--- src/uci/process.rs
+++ src/uci/process.rs
@@ -2,9 +2,87 @@ use std::io::LineBufferedWriter;
 use std::io::stdio::StdWriter;
 
 use super::types::CmdVal;
-use super::state::State;
+use super::state::{State, Mode};
 
-pub fn process(state: &mut State, curr_cmd: &CmdVal, output: &mut LineBufferedWriter<StdWriter>) {
-    // TODO implement uci command processing
-    unimplemented!()
+pub fn process(state: &mut State, cmd: &CmdVal, output: &mut LineBufferedWriter<StdWriter>) {
+    match *cmd {
+        CmdVal::Debug(val) => {
+            // TODO set debug flag
+            unimplemented!();
+        },
+        CmdVal::IsReady => {
+            // TODO implement IsReady
+            unimplemented!();
+        },
+        CmdVal::Register(ref param) => {
+            // TODO register
+            unimplemented!();
+        },
+        ref cmd => {
+            match state.mode {
+                Mode::Init => {
+                    if *cmd == CmdVal::Uci {
+                        // TODO print id/option/uciok
+                        state.mode = Mode::Wait;
+                        unimplemented!();
+                    }
+                },
+                Mode::Wait => {
+                    match *cmd {
+                        CmdVal::SetOption(ref name, ref val_opt) => {
+                            // TODO maybe initialize
+                            // TODO set options
+                            unimplemented!();
+                        },
+                        CmdVal::UciNewGame => {
+                            // TODO note that ucinewgame supported
+                            // TODO reset game status
+                            state.mode = Mode::NewGame;
+                            unimplemented!();
+                        },
+                        ref cmd @CmdVal::SetupPosition(..) => {
+                            if state.ucinewgame_support {
+                                // TODO set up position for same game
+                                state.mode = Mode::Ready;
+                                unimplemented!();
+                            } else {
+                                process(state, &CmdVal::UciNewGame, output);
+                                state.ucinewgame_support = false;
+                                process(state, cmd, output);
+                            }
+                        },
+                        _ => {},
+                    }
+                },
+                Mode::NewGame => {
+                    if let CmdVal::SetupPosition(ref pos, ref from_to_vec) = *cmd {
+                        // TODO setup position for new game
+                        state.mode = Mode::Ready;
+                        unimplemented!();
+                    }
+                },
+                Mode::Ready => {
+                    if let CmdVal::Go(ref param) = *cmd {
+                        // TODO start searching
+                        state.mode = Mode::Search;
+                        unimplemented!();
+                    }
+                },
+                Mode::Search => {
+                    match *cmd {
+                        CmdVal::PonderHit => {
+                            // TODO ponder hit
+                            unimplemented!();
+                        },
+                        CmdVal::Stop => {
+                            // TODO stop search
+                            // Also if time is out
+                            unimplemented!();
+                        },
+                        _ => {},
+                    }
+                },
+            }
+        },
+    }
 }

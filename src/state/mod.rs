21db9f34b1139f68bfe8c575efccee121657a523
Theemathas Chirananthavat <theemathas@gmail.com>
2015-01-03 10:07:41 +0700

    Move time-related stuff to uce::types

--- src/state/mod.rs
+++ src/state/mod.rs
@@ -2,20 +2,15 @@
 
 #![allow(dead_code)]
 
-use std::time::Duration;
 
-use game::{Position, Color, Move, NumMoves, NumPlies};
+use game::{Position, Move, NumMoves, NumPlies};
 use types::NumNodes;
-
-pub use self::TimeData::{TimeLeft, ExactTime};
 
 #[deriving(Clone)]
 pub struct State {
     pub is_debug: bool,
     pub pos: Position,
-    pub start_search: u64,
     pub search_param: SearchParam,
-    pub time_data: TimeData,
 }
 
 #[deriving(Clone)]
@@ -25,45 +20,4 @@ pub struct SearchParam {
     pub depth: Option<NumPlies>,
     pub nodes: Option<NumNodes>,
     pub mate: Option<NumMoves>,
-}
-
-#[deriving(Copy, Clone)]
-pub enum TimeData {
-    TimeLeft(TimeLeftData),
-    ExactTime(Duration),
-}
-
-#[deriving(Copy, Clone)]
-pub struct TimeLeftData {
-    pub w_time: Duration,
-    pub b_time: Duration,
-    pub w_inc: Duration,
-    pub b_inc: Duration,
-    pub moves_to_go: NumMoves,
-}
-impl TimeLeftData {
-    pub fn time(&self, c: Color) -> Duration {
-        match c {
-            Color::White => self.w_time,
-            Color::Black => self.b_time,
-        }
-    }
-    pub fn set_time(&mut self, c: Color, val: Duration) {
-        match c {
-            Color::White => self.w_time = val,
-            Color::Black => self.b_time = val,
-        }
-    }
-    pub fn inc(&self, c: Color) -> Duration {
-        match c {
-            Color::White => self.w_inc,
-            Color::Black => self.b_inc,
-        }
-    }
-    pub fn set_inc(&mut self, c: Color, val: Duration) {
-        match c {
-            Color::White => self.w_inc = val,
-            Color::Black => self.b_inc = val,
-        }
-    }
 }
--- src/uci/types/mod.rs
+++ src/uci/types/mod.rs
@@ -1,6 +1,11 @@
 pub use self::param::{RegisterParam, GoParam, IdParam, InfoParam, ScoreType};
 
-use game::{Position, Move, FromTo};
+use std::time::Duration;
+
+use game::{Position, Color, Move, FromTo, NumMoves};
+use state::State;
+
+use super::state::UciState;
 
 pub mod options;
 mod param;
@@ -31,3 +36,54 @@ pub enum Response {
     Info(Vec<InfoParam>),
 }
 
+// When starting program, start at state "Init"
+// Always allow commands "debug" and "isready"
+#[deriving(Clone)]
+pub struct UciData {
+    search_state: Option<State>,
+    uci_state: UciState,
+    start_search: Option<u64>,
+    start_move: Option<u64>,
+    time_data: TimeData,
+}
+
+#[deriving(Copy, Clone)]
+pub enum TimeData {
+    TimeLeft(TimeLeftData),
+    ExactTime(Duration),
+}
+
+#[deriving(Copy, Clone)]
+pub struct TimeLeftData {
+    pub w_time: Option<Duration>,
+    pub b_time: Option<Duration>,
+    pub w_inc: Option<Duration>,
+    pub b_inc: Option<Duration>,
+    pub moves_to_go: Option<NumMoves>,
+}
+impl TimeLeftData {
+    pub fn time(&self, c: Color) -> Option<Duration> {
+        match c {
+            Color::White => self.w_time,
+            Color::Black => self.b_time,
+        }
+    }
+    pub fn set_time(&mut self, c: Color, val: Option<Duration>) {
+        match c {
+            Color::White => self.w_time = val,
+            Color::Black => self.b_time = val,
+        }
+    }
+    pub fn inc(&self, c: Color) -> Option<Duration> {
+        match c {
+            Color::White => self.w_inc,
+            Color::Black => self.b_inc,
+        }
+    }
+    pub fn set_inc(&mut self, c: Color, val: Option<Duration>) {
+        match c {
+            Color::White => self.w_inc = val,
+            Color::Black => self.b_inc = val,
+        }
+    }
+}

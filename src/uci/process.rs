a056a45092f3274994079086feabeded4b74e708
Theemathas Chirananthavat <theemathas@gmail.com>
2015-01-03 11:45:19 +0700

    Actually use uci::State

--- src/uci/process.rs
+++ src/uci/process.rs
@@ -2,8 +2,9 @@ use std::io::LineBufferedWriter;
 use std::io::stdio::StdWriter;
 
 use super::types::CmdVal;
+use super::state::State;
 
-pub fn process(curr_cmd: &CmdVal, output: &mut LineBufferedWriter<StdWriter>) {
+pub fn process(state: &mut State, curr_cmd: &CmdVal, output: &mut LineBufferedWriter<StdWriter>) {
     // TODO implement uci command processing
     unimplemented!()
 }
--- src/uci/start.rs
+++ src/uci/start.rs
@@ -4,8 +4,10 @@ use std::io::LineBufferedWriter;
 use super::types::CmdVal;
 use super::parse::parse;
 use super::process::process;
+use super::state::State;
 
 pub fn start(input: &mut StdinReader, output: &mut LineBufferedWriter<StdWriter>) {
+    let mut state = State::new();
     let mut inbuf = input.lock();
     for x in inbuf.lines() {
         let s = x.unwrap();
@@ -13,7 +15,7 @@ pub fn start(input: &mut StdinReader, output: &mut LineBufferedWriter<StdWriter>
             if curr_cmd == CmdVal::Quit {
                 return;
             } else {
-                process(&curr_cmd, output);
+                process(&mut state, &curr_cmd, output);
             }
         }
     }

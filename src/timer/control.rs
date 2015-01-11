92f6317d3ccc59cd00575dc1a37fe4ea843c32f7
Theemathas Chirananthavat <theemathas@gmail.com>
2015-01-11 11:33:45 +0700

    Implement timer termination

--- src/timer/control.rs
+++ src/timer/control.rs
@@ -1,6 +1,6 @@
 use std::sync::mpsc::{Sender, Receiver};
 use std::time::Duration;
-use std::io::timer;
+use std::io::Timer;
 use std::cmp;
 
 use game::Color;
@@ -12,15 +12,23 @@ pub fn start(data: Data, c: Color, tx: Sender<uci::Cmd>, rx_kill: Receiver<()>) 
     match data {
         Data::Infinite => return,
         Data::Exact(val) => {
-            timer::sleep(val);
-            let _ = tx.send(uci::Cmd::Stop);
+            let mut timer = Timer::new().unwrap();
+            let rx_timer = timer.oneshot(val);
+            select!(
+                _ = rx_timer.recv() => { let _ = tx.send(uci::Cmd::Stop); },
+                _ = rx_kill.recv() => {}
+            )
         },
         Data::Remain(val) => {
             let base = val.time(c).unwrap_or(Duration::zero());
             let inc = val.inc(c).unwrap_or(Duration::zero());
-            timer::sleep(calc_time(base, inc));
-            // TODO terminate when received from rx_kill
-            let _ = tx.send(uci::Cmd::Stop);
+            let val = calc_time(base, inc);
+            let mut timer = Timer::new().unwrap();
+            let rx_timer = timer.oneshot(val);
+            select!(
+                _ = rx_timer.recv() => { let _ = tx.send(uci::Cmd::Stop); },
+                _ = rx_kill.recv() => {}
+            )
         },
     }
 }

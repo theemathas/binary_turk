674c8be5ff7fc43fea9ef15b550a6bd012cfd9b7
Theemathas Chirananthavat <theemathas@gmail.com>
2015-01-16 10:24:22 +0700

    Remove eval::Result::Draw

--- src/eval.rs
+++ src/eval.rs
@@ -11,7 +11,7 @@ use super::game::{Position, Color, Piece, PieceType};
 use super::game::{Pawn, King, Queen, Bishop, Knight, Rook};
 use super::game::{is_checkmated, is_stalemated};
 
-pub use self::Result::{Score, WinIn, LoseIn, Draw};
+pub use self::Result::{Score, WinIn, LoseIn};
 
 pub type ScoreUnit = i32;
 
@@ -31,7 +31,6 @@ pub enum Result {
     // WinIn(NumMoves(0)): already checkmated.
     // WinIn(NumMoves(1)): Will be immediately checkmated after any move.
     LoseIn(NumMoves),
-    Draw,
 }
 impl fmt::String for Result {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
@@ -39,17 +38,16 @@ impl fmt::String for Result {
             Result::Score(val)   => write!(f, "cp {}", val * CENTIPAWNS_PER_UNIT),
             Result::WinIn(val)   => write!(f, "mate {}", val.0 as i32),
             Result::LoseIn(val)  => write!(f, "mate {}", (val.0 as i32) * -1),
-            Result::Draw         => write!(f, "cp {}", 0),
         }
     }
 }
 
 /// Evaluates the position without searching.
-pub fn eval(p: &Position) -> Result {
+pub fn eval(p: &Position, draw_val: ScoreUnit) -> Result {
     if is_checkmated(p.clone()) {
         LoseIn(NumMoves(0))
     } else if is_stalemated(p.clone()) {
-        Draw
+        Score(draw_val)
     } else {
         let c = p.side_to_move();
         Score(p.piece_iter().map( |(piece, _pos)| val_for_color(piece, c) ).sum())

08869ebd402c139e8ca82d8f2821e12db4abf10f
Theemathas Chirananthavat <theemathas@gmail.com>
2015-01-16 10:20:43 +0700

    Derive PartialOrd and Ord for types

--- src/types.rs
+++ src/types.rs
@@ -1,9 +1,9 @@
 //! Types that do not fit in any module.
 
-#[derive(PartialEq, Eq, Copy, Clone, Show)]
+#[derive(PartialEq, Eq, PartialOrd, Ord, Copy, Clone, Show)]
 pub struct NumNodes(pub u64);
 
-#[derive(PartialEq, Eq, Copy, Clone, Show)]
+#[derive(PartialEq, Eq, PartialOrd, Ord, Copy, Clone, Show)]
 pub struct NumVariations(pub u32);
 
 #[derive(PartialEq, Eq, Copy, Clone, Show)]
@@ -12,8 +12,8 @@ pub struct PerMill(pub u32);
 #[derive(PartialEq, Eq, Copy, Clone, Show)]
 pub struct NumCpu(pub u32);
 
-#[derive(PartialEq, Eq, Copy, Clone, Show)]
+#[derive(PartialEq, Eq, PartialOrd, Ord, Copy, Clone, Show)]
 pub struct NumPlies(pub u32);
 
-#[derive(PartialEq, Eq, Copy, Clone, Show)]
+#[derive(PartialEq, Eq, PartialOrd, Ord, Copy, Clone, Show)]
 pub struct NumMoves(pub u32);

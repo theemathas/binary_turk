4f150d9db0aeaafeea5646ea3e14569c0e097b67
Theemathas Chirananthavat <theemathas@gmail.com>
2015-01-30 22:11:12 +0700

    Use new logging interface

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 extern crate time;
 #[macro_use]
 extern crate log;
+extern crate env_logger;
 
 use std::old_io as io;
 
@@ -14,5 +15,6 @@ mod timer;
 mod uci;
 
 fn main() {
+    env_logger::init().unwrap();
     uci::start(io::stdin(), io::stdout());
 }

05dfed27df8e9f1227e59bcf75e4cb8660ec94a3
Theemathas Chirananthavat <theemathas@gmail.com>
2015-02-07 16:59:39 +0700

    Add NumNodes to search data

--- src/search/types.rs
+++ src/search/types.rs
@@ -38,9 +38,15 @@ pub enum Cmd {
 
 // TODO put actual data here
 #[derive(Debug)]
-pub struct Data(());
+pub struct Data {
+    pub nodes: NumNodes,
+}
 impl Data {
-    pub fn one_node() -> Data { Data(()) }
-    pub fn combine(self, _: Data) -> Data { Data(()) }
-    pub fn increment(self) -> Data { Data(()) }
+    pub fn one_node() -> Data { Data { nodes: NumNodes(1) } }
+    pub fn combine(self, other: Data) -> Data {
+        Data { nodes: NumNodes(self.nodes.0 + other.nodes.0) }
+    }
+    pub fn increment(self) -> Data {
+        Data { nodes: NumNodes(self.nodes.0 + 1) }
+    }
 }

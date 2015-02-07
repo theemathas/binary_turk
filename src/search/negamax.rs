8227b8779154cf5a249037c70a1a5cfb2be41ab5
Theemathas Chirananthavat <theemathas@gmail.com>
2015-02-07 12:20:39 +0700

    Change negamax into imperative code

--- src/search/negamax.rs
+++ src/search/negamax.rs
@@ -29,29 +29,35 @@ pub fn negamax(pos: &mut Position, depth: NumPlies, param: Param,
     let ans_opt: Option<(Score, Move, Data)> = {
         let temp = pos.clone();
         let move_iter = temp.legal_iter();
-        let mut ans_iter = move_iter.map( |curr_move| {
+
+        let mut prev_ans_opt: Option<(Score, Move, Data)> = None;
+
+        for curr_move in move_iter {
             let new_param = Param { draw_val: -param.draw_val };
-            let (score, _next_best_move, data) = pos.with_move(&curr_move, move |new_pos| {
+            let (temp_score, _next_best_move, curr_data) = pos.with_move(&curr_move, move |new_pos| {
                 negamax(new_pos, NumPlies(depth.0 - 1), new_param, is_killed)
             });
-            (score.increment(), curr_move, data)
-        });
+            let curr_score = temp_score.increment();
 
-        ans_iter.next().map(move |first_ans|
-            ans_iter.fold(first_ans, |prev_ans, curr_ans| {
+            let new_ans = match prev_ans_opt {
+                None => (curr_score, curr_move, curr_data),
+                Some(prev_ans) => {
 
-                let (prev_score, prev_move, prev_data) = prev_ans;
-                let (curr_score, curr_move, curr_data) = curr_ans;
+                    let (prev_score, prev_move, prev_data) = prev_ans;
 
-                let combined_data = prev_data.combine(curr_data);
+                    let combined_data = prev_data.combine(curr_data);
 
-                if curr_score > prev_score {
-                    (curr_score, curr_move, combined_data)
-                } else {
-                    (prev_score, prev_move, combined_data)
-                }
-            }
-        ))
+                    if curr_score > prev_score {
+                        (curr_score, curr_move, combined_data)
+                    } else {
+                        (prev_score, prev_move, combined_data)
+                    }
+                },
+            };
+            prev_ans_opt = Some(new_ans);
+        }
+
+        prev_ans_opt
     };
 
     match ans_opt {
